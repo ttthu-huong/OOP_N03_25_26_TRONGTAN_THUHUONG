aeb92feef84f29cf0a15eded83b6c72d7dea2b6f
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-09-04 08:36:38 +0000

    test

--- ourProject/src/App.java
+++ ourProject/src/App.java
@@ -2,10 +2,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Lesson lesson = new Lesson("id01", "OOP");
+        
 
-        System.out.println(lesson.getLessonname());
+        testLesson.test();
 
-
+      
     }
 }
---
+++ ourProject/src/test/testLesson.java
@@ -0,0 +1,12 @@
+public class testLesson {
+
+    public static void test(){
+
+        Lesson lesson = new Lesson("id01", "OOP");
+
+        System.out.println(lesson.getLessonname());
+
+
+    }
+    
+}
